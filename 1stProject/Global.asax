af138ff83f70e1a69a051167f916bba2b3587108
abctutoral.com <abctutoral.com@gmail.com>
2016-11-25 15:27:35 +0600

    Test commit

---
+++ 1stProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_1stProject.MvcApplication" Language="C#" %>
---
+++ 1stProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(_1stProject.Startup))]
+namespace _1stProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
